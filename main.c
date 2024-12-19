5ac029b66e15e133e762640218a330ea83918ead
Taylan Mayckon <130873668+taylanmayckon@users.noreply.github.com>
2024-12-19 14:37:16 -0300

    Add: Biblioteca conversor_velocidade e teste estático de conversão na main (#7)
    Co-authored-by: Lucasferreira08 <102885528+Lucasferreira08@users.noreply.github.com>

--- main.c
+++ main.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
+#include "conversor_velocidade/conversor_velocidade.h"
 #include "../conversor_tempo/conversor_tempo.h"
 
 int main() {
     double valor;
+    double kmh = 100.0;
+    double ms = 27.78;
+    double mph = 62.14;
+  
+    // conversor velocidade
+    printf("%.2f km/h -> %.2f m/s\n", kmh, kmh_para_ms(kmh));
+    printf("%.2f km/h -> %.2f mph\n", kmh, kmh_para_mph(kmh));
+    printf("%.2f m/s -> %.2f mph\n", ms, ms_para_mph(ms));
+    printf("%.2f m/s -> %.2f km/h\n", ms, ms_para_kmh(ms));
+    printf("%.2f mph -> %.2f m/s\n", mph, mph_para_ms(mph));
+    printf("%.2f mph -> %.2f km/h\n", mph, mph_para_kmh(mph));
 
-    // Como compilar: gcc -I./conversor_tempo main.c ./conversor_tempo/conversor_tempo.c -o main
+    printf("\nPressione Enter para sair...");
+    getchar();  // Aguarda o usuário pressionar Enter
 
+    // conversor tempo
     printf("Digite um valor em segundos: ");
     scanf("%lf", &valor);
     printf("%.2lf segundos = %.2lf minutos\n", valor, segundos_para_minutos(valor));
