b08410898f4f2b3c01ab17a64fb340b0ae975d4f
Greique <reiquearaujo2@gmail.com>
2024-12-22 12:53:41 -0300

    conversor_massa

---
+++ conversor-massa/conversor_massa.h
@@ -0,0 +1,16 @@
+#ifndef CONVERSOR_MASSA_H
+#define CONVERSOR_MASSA_H
+
+// Função para converter gramas para quilogramas
+float gramas_para_quilogramas(float gramas);
+
+// Função para converter quilogramas para gramas
+float quilogramas_para_gramas(float quilogramas);
+
+// Função para converter quilogramas para toneladas
+float quilogramas_para_toneladas(float quilogramas);
+
+// Função para converter toneladas para quilogramas
+float toneladas_para_quilogramas(float toneladas);
+
+#endif // CONVERSOR_MASSA_H
